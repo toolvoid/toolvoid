530b85efd5ce4db89c3bfac2d25473a678025f00
toolvoid <0voidtool0@gmail.com>
2026-05-03 17:40:36 +0530

    fix: add @ path alias to jsconfig

--- app/resume/page.js
+++ app/resume/page.js
@@ -1,4 +1,4 @@
-import { getToolMetadata } from '@/lib/seoConfig'
+import { getToolMetadata } from '../../lib/seoConfig'
 import ResumeClient from './ResumeClient'
 
 export const metadata = getToolMetadata('resume')
