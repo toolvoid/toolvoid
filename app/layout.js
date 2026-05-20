7b12ad4b352ede069b39cac19cd0f32e59bebbb3
toolvoid <0voidtool0@gmail.com>
2026-05-21 01:18:47 +0530

    feat: add Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -29,6 +29,18 @@ export default function RootLayout({ children }) {
           {children}
         </AuthSessionProvider>
         <Script src="https://checkout.razorpay.com/v1/checkout.js" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9LZ4JMZ26L"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9LZ4JMZ26L');
+          `}
+        </Script>
         <SpeedInsights />
       </body>
     </html>
